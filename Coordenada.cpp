170e8aebf2aaa6f1f6af3f21d0f31f3949bf37f3
Luiscastro1994 <luisitocastro94@hotmail.com>
2016-11-26 10:36:43 -0300

    Update Coordenada.cpp

--- Coordenada.cpp
+++ Coordenada.cpp
@@ -1,10 +1,13 @@
 #include "Coordenada.h"
 
+Coordenada::Coordenada():lat(0),lon(0){}
 Coordenada::Coordenada(Nat n, Nat m){
 	lat = n;
 	lon = m;
 }
-
+bool Coordenada::operator == (const Coordenada &otro)const{
+	return this->latitud() == otro.lat and this->longitud() ==otro.lon;
+}
 const Nat& Coordenada::latitud() const {
 	return lat;
 }
@@ -22,7 +25,7 @@ Nat Coordenada::distEuclidea(const Coordenada& c) const {
 	else{
 		x = (this->lat - c.latitud()) * (this->lat - c.latitud());
 	}
-
+	
 	if(this->lon < c.longitud()){
 		y = (c.longitud() - this->lon) * (c.longitud() - this->lon);
 	}
@@ -33,11 +36,11 @@ Nat Coordenada::distEuclidea(const Coordenada& c) const {
 }
 
 Coordenada Coordenada::CoordenadaArriba() const {
-	return Coordenada(lat+1, lon);
+	return Coordenada(lat-1, lon);
 }
 
 Coordenada Coordenada::CoordenadaAbajo() const {
-	return Coordenada(lat-1, lon);
+	return Coordenada(lat+1, lon);
 }
 
 Coordenada Coordenada::CoordenadaALaDerecha() const {
@@ -48,45 +51,16 @@ Coordenada Coordenada::CoordenadaALaIzquierda() const{
 	return Coordenada(lat, lon-1);	
 }
 
-
-/*NO OLVIDAR SACAR EL MAIN AL FINALIZAR DE TESTEAR LA CLASE*/
-/*
-int main(){
-
-	Coordenada c1 = Coordenada(5,6);
-	Coordenada c2 = Coordenada(3,8);
-	Coordenada c3 = Coordenada(7,1);
-	Coordenada c4 = Coordenada(9,0);
-	Coordenada c5 = Coordenada(0,0);
-
-
-	//tests latitud() y longitud():
-	cout << "c1 = " << "(" << c1.latitud() << ", " << c1.longitud() << ")" << endl;
-	cout << "c2 = " << "(" << c2.latitud() << ", " << c2.longitud() << ")" << endl;
-	cout << "c3 = " << "(" << c3.latitud() << ", " << c3.longitud() << ")" << endl;
-	cout << "c4 = " << "(" << c4.latitud() << ", " << c4.longitud() << ")" << endl;
-	cout << "c5 = " << "(" << c5.latitud() << ", " << c5.longitud() << ")" << endl;
-
-	//tests distEuclidea():
-	cout << "distancia euclidea entre c1 y c2: " << c1.distEuclidea(c2) << endl;
-	cout << "distancia euclidea entre c2 y c1: " << c2.distEuclidea(c1) << endl;
-	cout << "distancia euclidea entre c3 y c5: " << c3.distEuclidea(c5) << endl;
-	cout << "distancia euclidea entre c4 y c3: " << c4.distEuclidea(c3) << endl;
-
-	//tests coordenadaArriba():
-	cout << "coordenada arriba de c1: " << "(" << c1.CoordenadaArriba().latitud() << 
-	", " << c1.CoordenadaArriba().longitud() << ")" << endl;
-
-	cout << "coordenada abajo de c1: " << "(" << c1.CoordenadaAbajo().latitud() << 
-	", " << c1.CoordenadaAbajo().longitud() << ")" << endl;
-
-	cout << "coordenada a la derecha de c1: " << "(" << c1.CoordenadaALaDerecha().latitud() << 
-	", " << c1.CoordenadaALaDerecha().longitud() << ")" << endl;
-
-	cout << "coordenada a la izquierda de c1: " << "(" << c1.CoordenadaALaIzquierda().latitud() << 
-	", " << c1.CoordenadaALaIzquierda().longitud() << ")" << endl;
-
-	return 0;
+Conj<Coordenada> Coordenada::Lindantes() const{
+	Conj<Coordenada> res;
+	Coordenada c(latitud(),longitud());
+	Coordenada abajo(c.CoordenadaAbajo().latitud(),c.CoordenadaAbajo().longitud());
+	Coordenada arriba(c.CoordenadaArriba().latitud(),c.CoordenadaArriba().longitud());
+	Coordenada derecha(c.CoordenadaALaDerecha().latitud(),c.CoordenadaALaDerecha().longitud());
+	Coordenada izquierda(c.CoordenadaALaIzquierda().latitud(),c.CoordenadaALaIzquierda().longitud());
+	res.Agregar(abajo);
+	res.Agregar(arriba);
+	res.Agregar(izquierda);
+	res.Agregar(derecha);
+	return res;
 }
-
-*/
