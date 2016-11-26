8f40d9c04f71cb242a858099f3f1837780d881fc
Luiscastro1994 <luisitocastro94@hotmail.com>
2016-11-26 11:01:02 -0300

    Update Mapa.h

--- Mapa.h
+++ Mapa.h
@@ -18,56 +18,60 @@
 #include "Coordenada.h"
 #include "Modulos-CPP/aed2/Conj.h"
 #include "Modulos-CPP/aed2/Vector.h"
-
 using namespace std;
 using namespace aed2;
 
 class Mapa{
-	public:
-		// Genera un mapa vacío.
-		Mapa();
-
-		// Agrega la coordenada al mapa.
-		void agregarCoord(Coordenada& c);
-
-		// Devuelve ancho de mapa.
-		const Nat& longitudMaxima() const;
-
-		// Devuelve altura de mapa.
-		const Nat& latitudMaxima() const;
-
-		// Devuelve las coordenadas del mapa.
-		const Conj<Coordenada>& coordenadasMapa() const;
-
-		// Dice si la coordenada pertenece al conjunto de coordenadas del mapa.
-		bool posExiste(Coordenada& c) const;
-
-		// Devuelve true si los dos caminos estan conectados.
-		bool hayCamino(Coordenada& c, Coordenada& c2) const; 
-
-		// Devuelve true si existe un camino que conecte a las coordenadas.
-		bool hayCaminoQ(Coordenada& c, Nat i, Nat j, Conj<Coordenada>& cs) const;
-
-		// Devuelve true si existe un camino que conecte a las coordenadas por arriba.
-		bool existeCaminoPorArriba(Coordenada& c, Conj<Coordenada>& cs) const;
-
-		// Devuelve true si existe un camino que conecte a las coordenadas por abajo.
-		bool existeCaminoPorAbajo(Coordenada& c, Conj<Coordenada>& cs) const;
-
-		// Devuelve true si existe un camino que conecte a las coordenadas por derecha.
-		bool existeCaminoPorDerecha(Coordenada& c, Conj<Coordenada>& cs) const;
-
-		// Devuelve true si existe un camino que conecte a las coordenadas por izquierda.
-		bool existeCaminoPorIzquierda(Coordenada& c, Conj<Coordenada>& cs) const;
-
-		// Crea una matriz con las coordenadas del mapa.
-		Vector<Vector<bool> > crearMatrizMapa(Coordenada& c) const;
-
-	private:
-		Vector<Vector<Vector<Vector<bool> >*> > matriz;
-		Nat longitudmaxima;
-		Nat latitudmaxima;
-		Conj<Coordenada> coordenadas;
-
+public:
+	// Genera un mapa vacío.
+	Mapa();
+	
+	// Agrega la coordenada al mapa.
+	void agregarCoord(Coordenada& c);
+	
+	// Devuelve ancho de mapa.
+	const Nat& longitudMaxima() const;
+	
+	// Devuelve altura de mapa.
+	const Nat& latitudMaxima() const;
+	
+	// Devuelve las coordenadas del mapa.
+	const Conj<Coordenada>& coordenadasMapa() const;
+	
+	// Dice si la coordenada pertenece al conjunto de coordenadas del mapa.
+	bool posExiste(Coordenada& c) const;
+	
+	// Devuelve true si los dos caminos estan conectados.
+	bool hayCamino(Coordenada c, Coordenada c2) const; 
+	
+	// Devuelve true si existe un camino que conecte a las coordenadas.
+	bool hayCaminoQ(Coordenada& c, Nat i, Nat j, Conj<Coordenada>& cs) const;
+	
+	// Devuelve true si existe un camino que conecte a las coordenadas por arriba.
+	bool existeCaminoPorArriba(Coordenada& c, Conj<Coordenada>& cs) const;
+	
+	// Devuelve true si existe un camino que conecte a las coordenadas por abajo.
+	bool existeCaminoPorAbajo(Coordenada& c, Conj<Coordenada>& cs) const;
+	
+	// Devuelve true si existe un camino que conecte a las coordenadas por derecha.
+	bool existeCaminoPorDerecha(Coordenada& c, Conj<Coordenada>& cs) const;
+	
+	// Devuelve true si existe un camino que conecte a las coordenadas por izquierda.
+	bool existeCaminoPorIzquierda(Coordenada& c, Conj<Coordenada>& cs) const;
+	
+	// Crea una matriz con las coordenadas del mapa.
+	Vector<Vector<bool> > crearMatrizMapa(Coordenada& c) const;
+	
+	Conj<Coordenada> Lindantes(Conj<Coordenada> c,Conj<Coordenada> coordenadas,Conj<Coordenada> res) const;
+	
+	Vector <Vector < bool> > MatrizDeFalse(Nat k, Nat j);
+	
+	void Rellenar(Vector <Vector < bool> >& matriz,Conj<Coordenada> linda);
+private:
+	Vector<Vector<Vector<Vector<bool> > > > matriz;
+	Nat longitudmaxima;
+	Nat latitudmaxima;
+	Conj<Coordenada> coordenadas;
+	
 };
 
